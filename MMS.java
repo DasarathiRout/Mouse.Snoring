fc9e9df84a7e13be50e9cb361d3247faf69b158d
Dasarathi Rout <dasarathi.rout@live.com>
2020-09-16 13:45:44 +0530

    Create MMS.java

---
+++ MMS.java
@@ -0,0 +1,22 @@
+// Mouse Movement Simulation: MMS MouseSnoring 
+import java.awt.MouseInfo; 
+import java.awt.Point; 
+import java.awt.Robot; 
+// START.CLASS 
+public class MMS { 
+   public static void main(String[] arrays) throws Exception { 
+       Robot energy = new Robot(); 
+       while (true) { 
+           System.out.println(); 
+           energy.delay(1000 * 60); 
+           Point pointOne = MouseInfo.getPointerInfo().getLocation(); 
+           Point pointTwo = pointOne; 
+           energy.mouseMove(pointOne.x + 1, pointOne.y + 1); 
+           energy.mouseMove(pointOne.x - 1, pointOne.y - 1); 
+           energy.mouseMove(pointTwo.x, pointOne.y); 
+           System.out.printf("MMS.MouseSnoring(d)",pointOne.x,pointOne.y); 
+           pointOne = MouseInfo.getPointerInfo().getLocation(); 
+       } 
+   } 
+} 
+// END.CLASS 
