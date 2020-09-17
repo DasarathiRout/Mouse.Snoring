8efdc509d258fc1e0b374de99dcb9332c60dd0d4
Dasarathi Rout <dasarathi.rout@live.com>
2020-09-17 13:10:33 +0530

    Update MMS.java
    System.gc()

--- MMS.java
+++ MMS.java
@@ -7,15 +7,16 @@ public class MMS {
    public static void main(String[] arrays) throws Exception { 
        Robot energy = new Robot(); 
        while (true) { 
-           System.out.println(); 
+           //System.out.println(); 
            energy.delay(1000 * 60); 
            Point pointOne = MouseInfo.getPointerInfo().getLocation(); 
            Point pointTwo = pointOne; 
            energy.mouseMove(pointOne.x + 1, pointOne.y + 1); 
            energy.mouseMove(pointOne.x - 1, pointOne.y - 1); 
            energy.mouseMove(pointTwo.x, pointOne.y); 
-           System.out.printf("MMS.MouseSnoring(d)",pointOne.x,pointOne.y); 
+           //System.out.printf("MMS.MouseSnoring(d)",pointOne.x,pointOne.y); 
            pointOne = MouseInfo.getPointerInfo().getLocation(); 
+           System.gc(); 
        } 
    } 
 } 
